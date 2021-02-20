3f18b0a1e1be0c23c555b195e8f0c44c41d16e56
rncn <doraidamon1rwfo@outlook.jp>
2021-02-20 02:50:26 +0000

    [change] .gitignore rules

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
+	"log"
 	"net/http"
 	"time"
 
@@ -10,7 +12,7 @@ import (
 )
 
 var (
-	address = flag.String("address", ":8080", "Server Listen Address")
+	address    = flag.String("address", ":8080", "Server Listen Address")
 	tsladdress = flag.String("tsladdress", ":8081", "Server Listen Address(TSL)")
 )
 
@@ -27,6 +29,7 @@ func main() {
 	flag.Parse()
 
 	router := Router()
+
 	serve := &http.Server{
 		Handler: router,
 		Addr:    *address,
@@ -34,12 +37,21 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	tslServe := &http.Server{
-		Handler: router,
-		Addr: 
+
+	//CertConfig
+	cert, err := tls.X509KeyPair(certPem, keyPem)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	tlsServe := &http.Server{
+		Handler:   router,
+		Addr:      *tsladdress,
+		TLSConfig: cfg,
 	}
 	//ListenAndServe
-	if err := serve.ListenAndServe(); err != nil {
+	if err := tlsServe.ListenAndServe(); err != nil {
 		panic(err)
 	}
 }
